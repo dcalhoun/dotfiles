e9373011f533dfca87d16a6fbe7ee56704cd9654
David Calhoun <dpcalhoun@gmail.com>
2017-09-30 12:39:45 -0500

    Update config

--- hyper.js
+++ hyper.js
@@ -5,15 +5,18 @@ module.exports = {
     colors: ['#000000', '#ff0000', '#33ff00', '#ffff00', '#0066ff', '#cc00ff', '#00ffff', '#d0d0d0',
       '#808080', '#ff0000', '#33ff00', '#ffff00', '#0066ff', '#cc00ff', '#00ffff', '#ffffff'],
     copyOnSelect: true,
-    css: '',
+    css: `
+      .tabs_title.tabs_title { display: block !important; } /* Snazzy: Disable hide single tab */
+      .terminal { opacity: 1 !important; } /* Snazzy: Disable blur opacity */
+    `,
     cursorColor: '#f81ce5',
     fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
     fontSize: 16,
     foregroundColor: '#fff',
     hyperTabs: {
       activityColor: 'salmon',
-      closeAlign: 'left',
-      tabIconsColored: true
+      tabIconsColored: true,
+      trafficButtons: true
     },
     modifierKeys: {
       altIsMeta: true
@@ -21,16 +24,17 @@ module.exports = {
     padding: '12px 14px',
     shell: '/bin/zsh',
     summon: {
-      hotkey: 'Ctrl+;'
+      hotkey: 'Ctrl+;',
       // hideDock: true,
       // hideOnBlur: true
     },
     termCSS: '',
+    updateChannel: 'canary',
     windowSize: [1440, 900]
   },
 
   localPlugins: [
-    'hyperterm-summon'
+    // 'hyperterm-summon'
   ],
 
   plugins: [
@@ -40,7 +44,7 @@ module.exports = {
     'hypercwd',
     'hyperlinks',
     'hyperterm-cursor',
-    'hyperterm-paste'
-    // 'hyperterm-summon'
+    'hyperterm-paste',
+    'hyperterm-summon'
   ]
 };
