09851149dd4ca8ea1657f5f906997267a595bb11
David Calhoun <dpcalhoun@gmail.com>
2018-06-30 15:22:40 -0500

    Update config

--- hyper.js
+++ hyper.js
@@ -42,8 +42,8 @@ module.exports = {
     shell: '/bin/zsh',
     summon: {
       hotkey: 'Ctrl+;',
-      hideDock: true,
-      hideOnBlur: true,
+      //hideDock: true,
+      //hideOnBlur: true,
     },
     termCSS: '',
     updateChannel: 'stable',
