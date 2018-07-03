cd29ae7603a82008ea7c11ecc3f00ba91c0d7ab4
David Calhoun <dpcalhoun@gmail.com>
2018-07-03 15:48:23 -0500

    [change] Improve Hyper and vim colors

--- hyper.js
+++ hyper.js
@@ -21,9 +21,6 @@ module.exports = {
       '#ffffff',
     ],
     copyOnSelect: true,
-    css: `
-      .terminal, .term_fit { opacity: 1 !important; } /* Snazzy: Disable blur opacity */
-    `,
     cursorColor: '#f81ce5',
     fontFamily:
       '"Operator Mono SSm", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
@@ -42,25 +39,17 @@ module.exports = {
     shell: '/bin/zsh',
     summon: {
       hotkey: 'Ctrl+;',
-      //hideDock: true,
-      //hideOnBlur: true,
     },
     termCSS: '',
     updateChannel: 'stable',
-    // windowSize: [400, 400],
     windowSize: [1440, 900],
   },
-
-  // localPlugins: ['hyperterm-summon'],
-
   plugins: [
     'hyper-search',
-    'hyper-tabs-enhanced',
     'hyperlinks',
-    'hyperterm-cursor',
     'hyperterm-paste',
     'hyperterm-summon',
     'hypercwd',
-    'hyper-snazzy',
+    'hyper-oceanic-next',
   ],
 };
