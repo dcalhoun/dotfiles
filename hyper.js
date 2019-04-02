a2d628fa7d79712fd0591eb69d3210b4e00ccb59
David Calhoun <dpcalhoun@gmail.com>
2019-04-02 13:39:39 -0500

    [change] Various changes

--- hyper.js
+++ hyper.js
@@ -1,60 +1,60 @@
 module.exports = {
   config: {
-    backgroundColor: '#000',
-    borderColor: '#333',
+    backgroundColor: "#000",
+    borderColor: "#333",
     colors: [
-      '#000000',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#d0d0d0',
-      '#808080',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#ffffff',
+      "#000000",
+      "#ff0000",
+      "#33ff00",
+      "#ffff00",
+      "#0066ff",
+      "#cc00ff",
+      "#00ffff",
+      "#d0d0d0",
+      "#808080",
+      "#ff0000",
+      "#33ff00",
+      "#ffff00",
+      "#0066ff",
+      "#cc00ff",
+      "#00ffff",
+      "#ffffff"
     ],
     copyOnSelect: true,
-    cursorColor: '#f81ce5',
+    cursorColor: "#f81ce5",
     css: `
       .tabs_title.tabs_title { display: block !important; } /* Snazzy: Disable hide single tab */
     `,
     fontFamily:
       '"Operator Mono SSm", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
     fontSize: 12,
-    foregroundColor: '#fff',
+    foregroundColor: "#fff",
     hypercwd: {},
     hyperTabs: {
-      activityColor: 'salmon',
+      activityColor: "salmon",
       tabIconsColored: true,
-      trafficButtons: true,
+      trafficButtons: true
     },
     modifierKeys: {
-      altIsMeta: true,
+      altIsMeta: true
     },
-    padding: '12px 14px',
-    shell: '/bin/zsh',
+    padding: "12px 14px",
+    shell: "/bin/zsh",
     summon: {
-      hotkey: 'Ctrl+;',
+      hotkey: "Ctrl+;",
       hideDock: true,
-      hideOnBlur: true,
+      hideOnBlur: true
     },
-    termCSS: '',
-    updateChannel: 'canary',
-    windowSize: [1440, 900],
+    termCSS: "",
+    updateChannel: "canary",
+    windowSize: [1440, 900]
   },
   plugins: [
-    'hyper-search',
-    'hyperlinks',
-    'hyperterm-paste',
-    'hyperterm-summon',
-    'hypercwd',
-    'hyper-snazzy',
+    "hyper-search",
+    "hyperlinks",
+    "hyperterm-paste",
+    "hyperterm-summon",
+    "hypercwd",
+    "hyper-snazzy"
   ]
 };
