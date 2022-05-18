490b22b4c19b474d41dd92104938ab3a71bbb60e
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-05-18 15:04:52 -0500

    Increase stability and clarity of install script

--- install.sh
+++ install.sh
@@ -1,31 +1,65 @@
 #!/bin/sh
 
-# Update default shell
-chsh -s $(which zsh)
-
 # Globals
 MY_DOTFILES=$HOME/.dotfiles
 TB_DOTFILES=$HOME/.dotfiles-thoughtbot
 
+# Update default shell
+if [ $(basename $SHELL) = "zsh" ]; then
+  echo "✅ Default shell is zsh."
+else
+  echo "🛠 Setting default shell to zsh..."
+  chsh -s $(which zsh)
+fi
+
 # Install thoughtbot/dotfiles
-if [ ! -e $TB_DOTFILES ]; then
+if [ -e $TB_DOTFILES ]; then
+  echo "✅ thoughtbot dotfiles installed."
+else
+  echo "🛠 Cloning Thoughtbot dotfiles..."
   git clone https://github.com/thoughtbot/dotfiles.git $TB_DOTFILES
 fi
 
 # Install tpm
-if [ ! -e ${MY_DOTFILES}/tmux/plugins/tpm ]; then
+if [ -e ${MY_DOTFILES}/tmux/plugins/tpm ]; then
+  echo "✅ tpm installed."
+else
+  echo "🛠 Installing tpm..."
   git clone https://github.com/tmux-plugins/tpm ${MY_DOTFILES}/tmux/plugins/tpm
 fi
 
+# Install Homebrew
+if which brew > /dev/null; then
+  echo "✅ Homebrew installed."
+else
+  echo "🛠 Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
 # Install Homebrew packages
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo "🛠 Installing Homebrew packages..."
 brew bundle --file=${MY_DOTFILES}/Brewfile
 
-# Install Node.js and pure-prompt
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install nodejs latest
-asdf global nodejs latest
-npm install --global pure-prompt
+# Install Node.js
+if which node > /dev/null; then
+  echo "✅ Node.js installed."
+else
+  echo "🛠 Installing Node.js..."
+  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf install nodejs latest
+  asdf global nodejs latest
+  asdf shell nodejs latest
+fi
+
+# Install pure-prompt
+package='pure-prompt'
+if [ `npm list -g | grep -c $package` -eq 1 ]; then
+  echo "✅ pure-prompt installed."
+else
+  echo "🛠 Installing pure-prompt..."
+  npm install $package --no-shrinkwrap
+fi
 
 # Link files with rcup
+echo "🔗 Linking dotfiles"
 env RCRC=$HOME/.dotfiles/rcrc rcup
